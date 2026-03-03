d4c4ad3613c69f2a3e3119208db948f8017ab9eb
jeperjesi <jeperjesi@leapdistrict.com>
2026-03-03 03:20:33 +0000

    add master fund list, reconfigure UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
 
-df = pd.DataFrame({
+BASE_DIR = Path(__file__).resolve().parent
+master_fund_list_path = BASE_DIR / "data" / "master_fund_list.csv"
+
+#script parameters (move to csv file?)
+df_params = pd.DataFrame({
     "Script": ["Pablo_script_1.py", "Pablo_script_2.py", "Joseph_script_1.py"],
     "Param1": ["File path", "File path", "URL"],
     "Param1 required": ["Yes", "Yes", "Yes"],
@@ -9,6 +14,9 @@ df = pd.DataFrame({
     "Param2 required": ["Yes", "Yes", "No"]
 })
 
+df_master_fund_list = pd.read_csv(master_fund_list_path)
+
+#script calls
 def run_script_a():
     st.write("Executing Pablo_script_1.py...")
     # do work
@@ -23,43 +31,64 @@ def run_script_c():
     result = Joseph_script_1.run_task(param1)
     st.success("Script finished")
     
-    # do work
 
 
 # UI items
 st.title("Leap District script runner")
+st.sidebar.write("App pages")
 st.write(
     "Select script and enter parameters in input boxes below."
 )
 
-script_options = ["Pablo_script_1.py", "Pablo_script_2.py", "Joseph_script_1.py"]
+script_options = ["Pablo_script_1.py", "Pablo_script_2.py", "Joseph_script_1.py", "nav_get_fund_list.py"]
 
 script = st.selectbox(
     "Select script to run",
     script_options
 )
 
+st.write("") 
+st.write("Global parameters")
 
-param1 = st.text_input(
+param_fund_name = st.selectbox("Fund name:", df_master_fund_list["fund_name"].unique())
+
+param_date_1 = st.date_input(
+    "Date:",
+    value=(pd.Timestamp.today().replace(day=1) - pd.Timedelta(days=1)
+).date()
+)
+
+st.write("") 
+st.write("Other parameters")
+
+param_freetext_1 = st.text_input(
     label="Enter parameter 1 (if applicable):",
     placeholder="/sites/LeapDistrict/Shared Documents/..."
 )
 
-param2 = st.text_input(
+param_freetext_2 = st.text_input(
     label="Enter parameter 2 (if applicable):",
     placeholder="/sites/LeapDistrict/Shared Documents/..."
 )
+
+param_date_2 = st.date_input(
+    "Date:"
+)
+
 st.write("")          # gap between input boxes and dataframe
 st.write("")          # gap between input boxes and dataframe
 
-st.dataframe(df, hide_index=True)
+st.write("Script parameters") 
+st.dataframe(df_params, hide_index=True)
+st.write("")
+st.write("API call reference") 
+st.dataframe(df_master_fund_list, hide_index=True)
 
 st.write("")          # gap between dataframe and output
 st.write("")          # gap between dataframe and output
 
 st.write("You selected:", script)
-st.write("You entered Parameter 1:", param1)
-st.write("You entered Parameter 2:", param2)
+
 
 if st.button("Run"):
     if script == "Pablo_script_1.py":
